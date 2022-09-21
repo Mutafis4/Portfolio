947b0389f1093bfd824c5e679fdbe50fb7cebd49
Mutafis4 <113209683+Mutafis4@users.noreply.github.com>
2022-09-21 19:45:35 -0400

    Update netpay.py

--- Projects/netpay.py
+++ Projects/netpay.py
@@ -1,9 +1,14 @@
 # Ask user how much they make an hour
 salary = int(input("How much do you make an hour? $"))
 # How many hours do you work
-hours_worked = int(input("How many hours do you work in a week? "))
+hours_worked = int(input("How many hours do you work in a week? Hours: "))
 # Which Tax Bracket are they in
 tax_percent = float(input("Which tax bracket are you in? "))
 # Total salary with hours worked
-gross = format(salary * hours_worked, ",.2f")
-print(gross)
+gross = float(salary * hours_worked)
+# Net pay
+take_home = (gross - (tax_percent / 100 * gross))
+# Print out net
+print("Your Net Salary is {:0.2f}".format(take_home))
+
+
